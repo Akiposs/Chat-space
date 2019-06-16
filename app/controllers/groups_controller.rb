8ed8b8c4d8eb83c1195bb66b32c7f9db53af9275
Akihiro_Takano <superakip1995@gmail.com>
2019-06-17 00:27:38 +0900

    miss_update2

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,13 +15,8 @@ class GroupsController < ApplicationController
       render :new
     end
   end
-  
-  def edit
-    @group = Group.find(params[:id])
-  end
 
-
-  def update
+   def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
