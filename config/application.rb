8164d0fdaa3535e027f9243d70e73e7308f0cfd2
Akihiro_Takano <superakip1995@gmail.com>
2019-06-22 16:22:53 +0900

    微更新

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,8 @@ module ChatSpace
       g.helper false
       g.test_framework false
       config.i18n.default_locale = :ja
-      config.action_view.automatically_disable_submit_tag = false 
+      config.time_zone = 'Tokyo'
+      config.active_record.default_timezone = :local
     end
   end
 end
